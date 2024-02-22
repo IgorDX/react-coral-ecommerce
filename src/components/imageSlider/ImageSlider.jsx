2d06baf509c9c67e4149674a2d4e8bcb5abaa3d1
Игорь <dorokhov.ihor@gmail.com>
2024-02-22 18:19:23 +0200

    Updates

---
+++ src/components/imageSlider/ImageSlider.jsx
@@ -0,0 +1,142 @@
+import React, { useEffect, useState, useRef } from 'react';
+import "./imageSlider.scss";
+
+const MAX_IMAGE_WIDTH = 600;
+
+export const ImageSlider = ({ slides }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [width, setWidth] = useState(MAX_IMAGE_WIDTH);
+    const [startTouchX, setStartTouchX] = useState(0);
+    const [isTouching, setIsTouching] = useState(false);
+    const [startX, setStartX] = useState(0);
+    const [isDown, setIsDown] = useState(false)
+    const sliderContainerRef = useRef(null);
+
+    const init = () => {
+        const sliderWidth = document.documentElement.clientWidth - 50;
+        setWidth(sliderWidth);
+    };
+
+    useEffect(() => {
+        setCurrentIndex(0); 
+    }, [slides]);
+
+    useEffect(() => {
+        init();
+        window.addEventListener('resize', init);
+        return () => {
+            window.removeEventListener('resize', init);
+        };
+    }, []);
+
+    const sliderLineStyle = ()=>({
+        width:  width < MAX_IMAGE_WIDTH ? width : MAX_IMAGE_WIDTH + 'px',
+        transform: `translateX(${-(currentIndex * (width < MAX_IMAGE_WIDTH ? width : MAX_IMAGE_WIDTH))}px)`
+    })
+    const imagesStyle = () => ({
+        width: width < MAX_IMAGE_WIDTH ? width + 'px' : MAX_IMAGE_WIDTH + 'px',
+        height: 'auto'
+    });
+
+    const handleMouseDown = (e) => {
+        e.preventDefault();
+        setIsDown(true);
+        if (sliderContainerRef.current) {
+            sliderContainerRef.current.classList.add('active');
+            setStartX(e.pageX - sliderContainerRef.current.offsetLeft);
+        }
+    };
+
+    const handleMouseLeave = (e) => {
+        e.preventDefault();
+        setIsDown(false);
+        if (sliderContainerRef.current) {
+            sliderContainerRef.current.classList.remove('active');
+        }
+    };
+
+    const handleMouseUp = (e) => {
+        e.preventDefault();
+        setIsDown(false);
+        if (sliderContainerRef.current) {
+            sliderContainerRef.current.classList.remove('active');
+        }
+    };
+    const handleSlideLeft = () => {
+        setCurrentIndex(prevIndex => prevIndex === 0 ? slides.length - 1 : prevIndex - 1);
+    };
+    
+    const handleSlideRight = ()=>{
+        setCurrentIndex(prevIndex => prevIndex === slides.length - 1 ? 0 : prevIndex + 1)
+    }
+    const handleMouseMove = (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - sliderContainerRef.current.offsetLeft;
+        const walk = x - startX;
+        const direction = walk > 0 ? 1 : -1;
+        setIsDown(false);
+        if (direction === -1) {
+            handleSlideRight();
+        } else if (direction === 1) {
+            handleSlideLeft();
+
+        }
+    };
+    
+    const handleTouchStart = (e) => {
+        setIsTouching(true);
+        setStartTouchX(e.touches[0].clientX);
+
+    };
+    
+    const handleTouchMove = (e) => {
+        if(!isTouching) return
+        const currentX = e.touches[0].clientX;
+
+        const diff = currentX - startTouchX;
+        setIsTouching(false);
+
+        if (diff > 0) {
+            handleSlideLeft();
+        } else if (diff < 0) {
+            handleSlideRight();
+        }
+    };
+    
+    const handleTouchEnd = () => {
+        setIsTouching(false);
+    };
+    return (
+        <div className="slider">
+            <div className='small-slider'>  
+                {slides.map((el, index) => (
+                    <div key={index} className='slider-item' onClick={() => setCurrentIndex(index)}>
+                        <img src={el} alt="image" />
+                    </div>  
+                ))}
+            </div>
+            <div
+    className='slider-container'
+    ref={sliderContainerRef}
+    onMouseDown={handleMouseDown}
+    onMouseMove={handleMouseMove}
+    onMouseUp={handleMouseUp}
+    onMouseLeave={handleMouseLeave}
+    onTouchStart={handleTouchStart}
+    onTouchMove={handleTouchMove}
+    onTouchEnd={handleTouchEnd}
+>
+                <div className='slider-line' style={sliderLineStyle()}>
+                    {slides.map((el, slideIndex) => (
+                        <div key={slideIndex} className='slide-item'>
+                            <img style={imagesStyle()} src={el} alt="Product" />
+                            <button className='arrowHolder left-arrow' onClick={handleSlideLeft}><img src="images/leftArrow.svg" alt="Left arrow" /></button>
+                            <button className='arrowHolder right-arrow' onClick={handleSlideRight}><img src="images/rightArrow.svg" alt="Right arrow" /></button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
---
+++ src/components/productCard/ProductCard.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import "./productCard.scss"
+import { Bounce, toast } from 'react-toastify';
+import {Link } from "react-router-dom"
+
+
+type Props = {
+    id:number;
+    name:string;
+    price: number;
+    category: string;
+    images: string[];
+    isFavorited : boolean;
+}
+export const ProductCard = (props : Props) => {
+  const [isFavorited, setIsFavorited] = useState(props.isFavorited);
+
+
+  const addToFavorite = () => {
+    setIsFavorited((prevIsFavorited) => {
+      const favoritedString = localStorage.getItem('favorited');
+      const favorited = favoritedString ? JSON.parse(favoritedString) : [];
+      if (prevIsFavorited) {
+        const updatedFavorited = favorited.filter((id) => id !== props.id);
+        toast.info('Item removed from favorite!', {
+          position: "top-center",
+          autoClose: 1000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+          });
+        localStorage.setItem('favorited', JSON.stringify(updatedFavorited));
+      } else {
+        toast.success('Item added to favorite!', {
+          position: "top-center",
+          autoClose: 1000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+          });
+        const updatedFavorited = [...favorited, props.id];
+        localStorage.setItem('favorited', JSON.stringify(updatedFavorited));
+      }
+      return !prevIsFavorited; 
+    });
+  };
+
+
+  return (
+    <div className='product-card'>
+        <div className="like">
+        <img onClick={addToFavorite} height={32} width={32} src={ isFavorited  ? "images/heart-like.svg" : "images/heart-unlike.svg"} alt="Like" />
+        </div>
+        <Link to={`/${props.id}`}>
+        <img src={props.images[0]} alt="Product iamge" />
+        <h3>{props.name}</h3>
+        <div className='bottom-card'>
+            <p>{props.category}</p>
+            <p>${props.price}</p>
+        </div>
+        </Link>
+    </div>
+  )
+}
