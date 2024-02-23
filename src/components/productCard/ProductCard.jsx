0ab9ef2199cb21537682f18caf76281f8bb95472
Игорь <dorokhov.ihor@gmail.com>
2024-02-23 14:24:47 +0200

    removed animation

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -6,25 +6,7 @@ import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const ProductCard = (props) => {
   const [isFavorited, setIsFavorited] = useState(props.isFavorited);
-  const navigate = useNavigate();
-  useEffect(() => {
-    const scrollToTop = () => {
-      const scrollStep = window.scrollY / 50;
-      const scrollInterval = setInterval(() => {
-        if (window.scrollY !== 0) {
-          window.scrollTo(0, window.scrollY - scrollStep);
-        } else {
-          clearInterval(scrollInterval); 
-        }
-      }, 1); 
-    };
-  
-    scrollToTop(); 
-  
-    return () => {
-    };
-  }, []);
-  
+
 
   const addToFavorite = () => {
     setIsFavorited((prevIsFavorited) => {
@@ -69,7 +51,7 @@ export const ProductCard = (props) => {
         <div className="like">
         <img onClick={addToFavorite} height={32} width={32} src={ isFavorited  ? "images/heart-like.svg" : "images/heart-unlike.svg"} alt="Like" />
         </div>
-        <Link to={`/${props.id}`}>
+        <Link to={`/${props.id}`} >
         <img src={props.images[0]} alt="Product iamge" />
         <h3>{props.name}</h3>
         <div className='bottom-card'>
